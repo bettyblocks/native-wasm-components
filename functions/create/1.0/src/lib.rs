7f965f3e0ce58302d3cd160941fa685c75a55465
J. Quak <jordy.quak@bettyblocks.com>
2025-10-14 15:29:08 +0200

    chore: add helper-context to params

--- functions/create/1.0/src/lib.rs
+++ functions/create/1.0/src/lib.rs
@@ -5,20 +5,13 @@ use crate::data_api::crud::crud::{create as crud_create, HelperContext};
 use crate::exports::betty_blocks::create::create::{Guest, Input, JsonString};
 
 impl Guest for Create {
-    fn create(input: Input) -> Result<JsonString, String> {
-        let helper_context = HelperContext {
-            application_id: "application_id".to_string(),
-            action_id: "action_id".to_string(),
-            log_id: "log_id".to_string(),
-            encrypted_configurations: None,
-            jwt: None,
-        };
+    fn create(helper_context: HelperContext, input: Input) -> Result<JsonString, String> {
         let validates = match input.validates {
             true => vec!["default".to_string()],
             false => vec!["empty".to_string()],
         };
         let response = crud_create(&helper_context, &input.model, &input.mapping, Some(&validates));
-        response
+        Ok(response?)
     }
 }
 
