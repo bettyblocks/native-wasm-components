d512773f46339313ce1a28435dff7401e05c61e4
Mees Molenaar <mees.molenaar@bettyblocks.com>
2025-10-31 10:50:35 +0100

    fix: correctly handle when no data is send

--- functions/delete/1.0/src/lib.rs
+++ functions/delete/1.0/src/lib.rs
@@ -5,13 +5,16 @@ use crate::betty_blocks::crud::crud::{delete as crud_delete, HelperContext};
 use crate::exports::betty_blocks::delete::delete::{Guest, Input, JsonString};
 
 impl Guest for Delete {
-    fn create(helper_context: HelperContext, input: Input) -> Result<JsonString, String> {
-        let response = crud_delete(
-            &helper_context,
-            &input.record.model,
-            &input.record.data.id.to_string(),
-        );
-        Ok(response?)
+    fn delete(helper_context: HelperContext, input: Input) -> Result<JsonString, String> {
+        if let Some(data) = &input.record.data {
+            Ok(crud_delete(
+                &helper_context,
+                &input.record.model,
+                &data.id.to_string(),
+            )?)
+        } else {
+            Err("Record does not exist".to_string())
+        }
     }
 }
 
--- functions/update/1.0/src/lib.rs
+++ functions/update/1.0/src/lib.rs
@@ -6,19 +6,22 @@ use crate::exports::betty_blocks::update::update::{Guest, Input, JsonString};
 
 impl Guest for Update {
     fn update(helper_context: HelperContext, input: Input) -> Result<JsonString, String> {
-        let validates = match input.validates {
-            true => vec!["default".to_string()],
-            false => vec!["empty".to_string()],
-        };
+        if let Some(data) = &input.selected_record.data {
+            let validates = match input.validates {
+                true => vec!["default".to_string()],
+                false => vec!["empty".to_string()],
+            };
 
-        let response = crud_update(
-            &helper_context,
-            &input.selected_record.model,
-            &input.selected_record.data.id.to_string(),
-            &input.mapping,
-            Some(&validates),
-        );
-        Ok(response?)
+            Ok(crud_update(
+                &helper_context,
+                &input.selected_record.model,
+                &data.id.to_string(),
+                &input.mapping,
+                Some(&validates),
+            )?)
+        } else {
+            Err("Record does not exist".to_string())
+        }
     }
 }
 
