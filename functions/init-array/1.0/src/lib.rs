e584b2dd5824a452ce745e88fbe4605cabc4d1b8
Robin Walthuis <robinwalthuis@hotmail.com>
2026-02-24 15:56:49 +0100

    feat: create init-array component

---
+++ functions/init-array/1.0/src/lib.rs
@@ -0,0 +1,16 @@
+use crate::exports::betty_blocks::init_array::init_array::{Guest, Output};
+
+wit_bindgen::generate!({ generate_all });
+
+struct Component;
+
+impl Guest for Component {
+    fn init_array() -> Output {
+        Output {
+            result: String::from("[]")
+        }
+    }
+}
+
+export! {Component}
+    
