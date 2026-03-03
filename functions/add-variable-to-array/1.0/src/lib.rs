95482358152ca5ca2787d82a6fa1dab9246f5a10
Robin Walthuis <robinwalthuis@hotmail.com>
2026-03-03 08:43:06 +0100

    fix: have everything use supported types
    No more lists of json-strings, and stuff like that. This way things work nicer with the actions compiler, and being less optimized here doesn't matter because the functions will be inlined anyway.

--- functions/add-variable-to-array/1.0/src/lib.rs
+++ functions/add-variable-to-array/1.0/src/lib.rs
@@ -1,5 +1,5 @@
 use crate::exports::betty_blocks::add_variable_to_array::add_variable_to_array::{
-    Guest, JsonString, Output,
+    Guest, JsonString,
 };
 
 wit_bindgen::generate!({ generate_all });
@@ -7,9 +7,18 @@ wit_bindgen::generate!({ generate_all });
 struct Component;
 
 impl Guest for Component {
-    fn add_variable_to_array(mut collection: Vec<JsonString>, item: JsonString) -> Output {
-        collection.push(item);
-        Output { result: collection }
+    fn add_variable_to_array(
+        collection_json: JsonString,
+        item: JsonString,
+    ) -> Result<JsonString, String> {
+        let mut collection: Vec<serde_json::Value> = serde_json::from_str(&collection_json)
+            .map_err(|_err| String::from("Arguments were not correctly formatted"))?;
+        collection.push(
+            serde_json::from_str(&item)
+                .map_err(|_err| String::from("Arguments were not correctly formatted"))?,
+        );
+        serde_json::to_string(&collection)
+            .map_err(|_err| String::from("Could not stringify collection"))
     }
 }
 
