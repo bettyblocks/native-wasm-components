16030976e56bf47589fa2eef5759e2493f81d250
Robin Walthuis <robinwalthuis@hotmail.com>
2026-03-09 09:47:05 +0100

    fix: fix expected map format on create object

--- functions/create-object/1.0/src/lib.rs
+++ functions/create-object/1.0/src/lib.rs
@@ -8,12 +8,21 @@ struct Component;
 
 impl Guest for Component {
     fn create_object(key_value_map: JsonString) -> Result<JsonString, String> {
-        let parsed_map: Vec<(serde_json::Value, serde_json::Value)> =
-            serde_json::from_str(&key_value_map)
-                .map_err(|_err| String::from("Arguments were not correctly formatted!"))?;
-        let hashmap = parsed_map
-            .into_iter()
-            .collect::<HashMap<serde_json::Value, serde_json::Value>>();
+        let parsed_map: Vec<serde_json::Value> = serde_json::from_str(&key_value_map)
+            .map_err(|_err| String::from("Arguments were not correctly formatted"))?;
+
+        let mut hashmap = HashMap::new();
+
+        for mut key_value in parsed_map {
+            hashmap.insert(
+                key_value
+                    .get_mut("key")
+                    .ok_or_else(|| String::from("Key value map was missing a key"))?.take(),
+                key_value
+                    .get_mut("value")
+                    .ok_or_else(|| String::from("Key value map was missing a value"))?.take(),
+            );
+        }
 
         serde_json::to_string(&hashmap)
             .map_err(|_err| String::from("Could not stringify collection"))
