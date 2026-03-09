1785a91ad5ad4d8d685a70598590ed3df6401c57
Robin Walthuis <robinwalthuis@hotmail.com>
2026-03-09 09:47:05 +0100

    fix: fix expected map format on create object

--- functions/create-object/1.0/src/lib.rs
+++ functions/create-object/1.0/src/lib.rs
@@ -7,16 +7,8 @@ wit_bindgen::generate!({ generate_all });
 struct Component;
 
 impl Guest for Component {
-    fn create_object(key_value_map: JsonString) -> Result<JsonString, String> {
-        let parsed_map: Vec<(serde_json::Value, serde_json::Value)> =
-            serde_json::from_str(&key_value_map)
-                .map_err(|_err| String::from("Arguments were not correctly formatted!"))?;
-        let hashmap = parsed_map
-            .into_iter()
-            .collect::<HashMap<serde_json::Value, serde_json::Value>>();
-
-        serde_json::to_string(&hashmap)
-            .map_err(|_err| String::from("Could not stringify collection"))
+    fn create_object(key_value_map: JsonString) -> JsonString {
+        key_value_map
     }
 }
 
