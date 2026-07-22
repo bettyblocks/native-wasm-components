964e7f21e8f08d5f0d314a710962f25b1dd60f01
J. Quak <jordy.quak@bettyblocks.com>
2026-07-22 10:11:48 +0200

    fix: update types

--- functions/create/1.0/src/lib.rs
+++ functions/create/1.0/src/lib.rs
@@ -4,7 +4,7 @@ wit_bindgen::generate!({ generate_all });
 use crate::betty_blocks_types::crud::crud::create as crud_create;
 use crate::betty_blocks_types::data_api::data_api::HelperContext;
 use crate::betty_blocks_types::types::types::{BettyModel, BettyPropertyMapping};
-use crate::exports::betty_blocks::create::create::{Guest, JsonString};
+use crate::exports::betty_blocks::create::create::{BettyRecordJson, Guest};
 
 impl Guest for Create {
     fn create(
@@ -12,7 +12,7 @@ impl Guest for Create {
         model: BettyModel,
         mapping: BettyPropertyMapping,
         validates: bool,
-    ) -> Result<JsonString, String> {
+    ) -> Result<BettyRecordJson, String> {
         let validates = match validates {
             true => vec!["default".to_string()],
             false => vec!["empty".to_string()],
